75beccd529258d358d80dcac07ef9a409a92be51
David Kelso <david@kelso.id.au>
2012-09-06 21:23:13 -0700

    Fix LED ordering to match PCBs.

--- slave/conf.h
+++ slave/conf.h
@@ -13,27 +13,27 @@
 
 #define LED1 PORTB
 #define LED1_MASK 0x07
-#define BLUE1 0
-#define GREEN1 1
-#define RED1 2
+#define BLUE1 1
+#define GREEN1 2
+#define RED1 0
 
 #define LED2 PORTB
 #define LED2_MASK 0x38
-#define BLUE2 3
-#define GREEN2 4
-#define RED2 5
+#define BLUE2 4
+#define GREEN2 5
+#define RED2 3
 
 #define LED3 PORTC
 #define LED3_MASK 0x07
-#define BLUE3 0
-#define GREEN3 1
-#define RED3 2
+#define BLUE3 1
+#define GREEN3 2
+#define RED3 0
 
 #define LED4 PORTC
-#define LED4_MASK 0xf8
-#define BLUE4 3
-#define GREEN4 4
-#define RED4 5
+#define LED4_MASK 0x38
+#define BLUE4 4
+#define GREEN4 5
+#define RED4 3
 
 #define MAX_PACKET_SIZE 128
 
@@ -43,4 +43,4 @@
 // Has to come after definition of F_CPU
 #include <util/delay.h>
 
-#define _set_with_mask(dest, mask, in) dest = (dest & ~mask) | in
+#define _set_with_mask(dest, mask, in) dest = (dest & ~mask) | (in & mask)
